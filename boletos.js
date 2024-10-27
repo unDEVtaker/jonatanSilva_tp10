cb1bf51bbfe2642694613f3e518a4b475c2da604
Jona <jonatanemanuelsilva6@gmail.com>
2024-10-26 21:38:49 -0300

    add boleto.js

---
+++ boletos.js
@@ -0,0 +1,66 @@
+// Consigna
+// Para llevar un control automatizado en la venta de boletos para ingresar a un
+// partido de fútbol, el Tech Leader de tu equipo te asigna la responsabilidad de
+// crear y asignar valor a un conjunto de variables. Recordá tener presente el tipo de
+// dato que contienen.
+// A continuación te dejamos algunos ejemplos de variables que puedes crear:
+// nombre - apellido - edad - número de teléfono - socio (valor booleano) - fecha del partido - hora del partido - número de silla - precio del boleto (con valores decimales).
+// 4. Usando el console.log() muestra al usuario el valor asignado a cada variable.
+// 5. Finalmente, utilizando el console.log() y el typeof(), muestra al usuario el tipo de
+// dato de cada una de las variables utilizadas.
+
+let nombre = "Jonatan";
+let apellido = "Silva";
+let edad = 36;
+let numeroTelefono = 1122333445566;
+let socio = true;
+let fechaPartido = "26-10-2024";
+let horaPartido = "21:00";
+let numeroAsiento = 10;
+let precioBoleto = 35000.03;
+let equipoLocal = "Independiente";
+let equipoVisita = "Racing";
+let estadio = "Libertadores de America";
+let puertaAcceso = "C2";
+let sector = "Platea baja";
+
+//mostrando los valores de cada variable.
+
+console.log(`Nombre: ${nombre}`);
+console.log(`Apellido: ${apellido}`);
+console.log(`Edad: ${edad}`);
+console.log(`Telefono de contancto: ${numeroTelefono}`);
+console.log(`Socio activo?: ${socio}`);
+console.log(`Fecha de partido: ${fechaPartido}`);
+console.log(`Hora de partido: ${horaPartido}`);
+console.log(`Numero de asiento: ${numeroAsiento}`);
+console.log(`Precio Boleto: ${precioBoleto}`);
+console.log(`Equipo Local: ${equipoLocal}`);
+console.log(`Equipo Visita: ${equipoVisita}`);
+console.log(`Estadio: ${estadio}`);
+console.log(`Acceso: ${puertaAcceso}`);
+console.log(`Sector: ${sector}`);
+
+//tipo de dato de cada una de las variables
+
+console.log("------tipo de variable------");
+console.log('Tipo de dato de varibale "nombre":', typeof nombre);
+console.log('Tipo de dato de varibale "apellido":', typeof apellido);
+console.log('Tipo de dato de varibale "edad":', typeof edad);
+console.log('Tipo de dato de varibale "numeroTelefono":', typeof numeroTelefono);
+console.log('Tipo de dato de varibale "socio":', typeof socio);
+console.log('Tipo de dato de varibale "fechaPartido":', typeof fechaPartido);
+console.log('Tipo de dato de varibale "horaPartido":', typeof horaPartido);
+console.log('Tipo de dato de varibale "numeroAsiento":', typeof numeroAsiento);
+console.log('Tipo de dato de varibale "precioBoleto":', typeof precioBoleto);
+console.log('Tipo de dato de varibale "equipoLocal":', typeof equipoLocal);
+console.log('Tipo de dato de varibale "equipoVisita":', typeof equipoVisita);
+console.log('Tipo de dato de varibale "estadio":', typeof estadio);
+console.log('Tipo de dato de varibale "puertaAcceso":', typeof puertaAcceso);
+console.log('Tipo de dato de varibale "sector":', typeof sector);
+
+
+
+
+
+
